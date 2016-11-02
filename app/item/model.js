88108331530ff3a444d5d91e0d5fc37af2d18e5a
Jeffrey Horn <jeffh@generalassemb.ly>
2016-11-02 14:45:07 -0400

    Add relationship between item and list

--- app/item/model.js
+++ app/item/model.js
@@ -3,4 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   text: DS.attr('string'),
   done: DS.attr('boolean'),
+  list: DS.belongsTo('list'), // not a number (id), but a real object!
+  // list_id: DS.attr('number') // OOOOH so close! but you're fucked
 });
