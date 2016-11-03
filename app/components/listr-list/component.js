d82568bf2a5e3dba8ac807b089bbfc4774e65421
Jeffrey Horn <jeffh@generalassemb.ly>
2016-11-03 16:52:26 -0400

    Remove toggleListDetail functionality

--- app/components/listr-list/component.js
+++ app/components/listr-list/component.js
@@ -2,13 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['listr'],
-  classNameBindings: ['listDetailHidden'],
-  listDetailHidden: false,
-  actions: {
-    toggleListDetail () {
-      return this.toggleProperty('listDetailHidden');
-    },
 
+  actions: {
     toggleItemDone (item) {
       return this.sendAction('toggleItemDone', item);
     },
